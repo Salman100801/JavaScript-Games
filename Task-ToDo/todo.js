18c827d61cb25cb27e17f65817c30ecd6a70ac1d
Salman Gafar Bagwan <salmanbagwan456@gmail.com>
2021-11-16 04:01:33 +0530

    Edit button added

--- Task-ToDo/todo.js
+++ Task-ToDo/todo.js
@@ -9,29 +9,48 @@ btn.addEventListener('click', (e) => {
     e.preventDefault();
     const li = document.createElement('li');
     const btnDone = document.createElement('button');
+    const btnEdit = document.createElement('button');
+    li.classList.add("text");
+    btnEdit.classList.add("btnEdit");
+    btnEdit.innerHTML = "Edit";
     btnDone.classList.add("btnDone");
     btnDone.innerHTML = "Done";
     if(task.value === ''){
         alert('Input Text first');
     }
     else{
+        //Add task to Ongoing
         li.appendChild(document.createTextNode(`${task.value}`));
         onGoing.appendChild(li);
+        onGoing.appendChild(btnEdit);
         onGoing.appendChild(btnDone);
         task.value = '';
 
+        //Edit Node
+         btnEdit.addEventListener('click', (e) => {
+            e.preventDefault();
+            task.value = `${li.textContent}`;
+            li.remove();
+            btnEdit.remove();
+            btnDone.remove();
+        });
+
+        //Task Completed
         btnDone.addEventListener('click' , (e) =>{
             e.preventDefault();
             completed.appendChild(li);
+            completed.appendChild(btnEdit);
             completed.appendChild(btnDone);
-            btnDone.innerHTML = "Delete";
+            const Done = btnDone;
+            btnEdit.innerHTML = "Edit Task"
+            Done.innerHTML = "Delete";
             //Deleting Node
-            btnDone.addEventListener('click', (e) => {
+            Done.addEventListener('click', (e) => {
                 e.preventDefault();
                 li.remove();
+                btnEdit.remove();
                 btnDone.remove();
             });
-            
         });
         
     }
